e3cfb209ddfa6be61e32c6b7e050264f4f485811
Prabhaker Gautam <72571185+PrabhakerGautam@users.noreply.github.com>
2024-03-28 13:45:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,8 +150,9 @@ def main():
                 del df['Low']
                 del df['Adj Close']
                 del df['Volume']
-                train = df[(df.index >= '2014-01-01') & (df.index <= end_date)]['Close']
-                test =  df[(df.index >= start_date) & (df.index <= end_date)]['Close']
+                size = int(0.99*(len(df['Close'])))
+                train = df[:size]
+                test =  df[size:]
                 #st.write(test.tail())
                 #HORIZON = st.sidebar.number_input('Enter forecast horizon (in days):')
                 TRAIN_LEN = len(train)
